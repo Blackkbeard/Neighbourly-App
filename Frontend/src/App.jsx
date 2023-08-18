b8d528d432b139434b7357b0f28478a9e936b46d
Desmond Tong <tysdesmond2@gmail.com>
2023-08-18 11:00:43 +0800

    Setup basic router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <div>
-      <h2>PROJHECT 3 RABAK</h2>
+    <div className="container">
+      
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+      </Routes>
     </div>
   );
 }
