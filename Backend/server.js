a16457a2b6e81ddf1932bbbdebebefc8c94ead05
Blackkbeard <rvinesh2011@gmail.com>
2023-08-18 14:30:53 +0800

    Server js update
    Import of Auth to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,11 @@ const cors = require("cors"); //enables anyone to use the api
 const helmet = require("helmet"); //
 const rateLimit = require("express-rate-limit");
 const connectDB = require("./src/db/db"); //import database
+
 connectDB();
+
 const listings = require("./src/routers/listings");
+const auth = require("./src/routers/auth");
 
 //allows api to be called 100 times within 15min interval
 const limit = rateLimit({
@@ -24,6 +27,7 @@ app.use(limit);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", listings);
+app.use("/auth", auth);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
