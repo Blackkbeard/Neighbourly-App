7f91323c7509f2710d7fc6011ab1366aecd854f9
Blackkbeard <rvinesh2011@gmail.com>
2023-08-18 17:47:08 +0800

    Authentication
    Addition of fields for schema

--- Backend/src/models/Auth.js
+++ Backend/src/models/Auth.js
@@ -4,6 +4,10 @@ const AuthSchema = new mongoose.Schema(
   {
     email: { type: String, required: true },
     hash: { type: String, required: true },
+    display_name: { type: String, required: true },
+    location: { type: String, default: "", required: true }, //default location is ""?  to hardcode into options
+    postal_code: { type: Number, default: "", required: true },
+    biography: { type: String, default: "", required: true },
     created_at: { type: Date, required: true, default: Date.now },
   },
   { collection: "auth" }
