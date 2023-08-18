447202800a9d0d3cdfc5704fea6978b0812464ca
Desmond Tong <tysdesmond2@gmail.com>
2023-08-18 14:17:37 +0800

    Add conditional rendering to avatar & burger menu

--- Frontend/src/components/TopBar.jsx
+++ Frontend/src/components/TopBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,8 +6,29 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import GiteIcon from "@mui/icons-material/Gite";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Avatar from "@mui/material/Avatar";
+
+const navBar = [
+  "The Neighbourhood",
+  "Profile",
+  "Transaction",
+  "Settings",
+  "Log Out",
+];
 
 const TopBar = (props) => {
+  const [anchorElUser, setAnchorElUser] = useState(false);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(false);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -25,18 +46,49 @@ const TopBar = (props) => {
             neighbourly
           </Typography>
 
-          {/* conditional rendering of burger menu */}
+          {/* conditional rendering of avatar & burger menu */}
           {props.showBurger && (
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 1 }}
-            >
-              <MenuIcon />
-            </IconButton>
+            <>
+              <IconButton sx={{ p: 1 }}>
+                <Avatar />
+              </IconButton>
+
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 1 }}
+                onClick={handleOpenUserMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+            </>
           )}
+
+          {/* navigavtion bar */}
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {navBar.map((item) => (
+              <MenuItem key={item} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{item}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
