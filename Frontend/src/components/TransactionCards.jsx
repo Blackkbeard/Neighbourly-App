acfe70cc74a6cf719d6a0d51a056c703353bfbc2
hweebie <limyinhwee@gmail.com>
2023-08-21 12:04:08 +0800

    Create Transaction card

---
+++ Frontend/src/components/TransactionCards.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import Grid from "@mui/material/Unstable_Grid2";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardHeader,
+  Typography,
+  CardActionArea,
+  Avatar,
+  Tooltip,
+  IconButton,
+  Box,
+} from "@mui/material";
+
+const Listings = () => {
+  return (
+    <>
+      {[1, 2, 3, 4].map((item, id) => {
+        return (
+          <Grid xs={12} key={id}>
+            <Card
+              sx={{ display: "flex", justifyContent: "space-between" }}
+              variant="outlined"
+              style={{
+                borderRadius: "1rem",
+                margin: "1rem",
+              }}
+            >
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Avatar sx={{ width: "3rem", height: "3rem" }}></Avatar>
+                </CardContent>
+              </Box>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h6">
+                    Listing Title
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    RequesterName
+                  </Typography>
+                </CardContent>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 100 }}
+                image="/sample-image.webp"
+                alt="Listing img"
+              />
+            </Card>
+          </Grid>
+        );
+      })}
+    </>
+  );
+};
+
+export default Listings;
--- Frontend/src/pages/Transactions.jsx
+++ Frontend/src/pages/Transactions.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { React, useContext } from "react";
 import TopBar from "../components/TopBar";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Container, Typography, Box } from "@mui/material";
 import Btn from "../components/Btn";
+import UserContext from "../context/user";
+import TransactionCard from "../components/TransactionCards";
 
 const Transactions = () => {
+  const userCtx = useContext(UserContext);
   return (
     <>
       <TopBar showBurger={true}></TopBar>
@@ -12,14 +15,28 @@ const Transactions = () => {
       <Container maxWidth="lg">
         <Box>
           <Grid container>
-            <Grid xs={12} style={{ borderStyle: "solid" }}>
-              <Typography textAlign="center">Transaction</Typography>
+            <Grid xs={12}>
+              <Typography variant="h5" textAlign="start" margin="2rem 0">
+                Your transactions
+              </Typography>
             </Grid>
-            <Grid xs={5} style={{ borderStyle: "solid" }}>
-              <Typography textAlign="center">Transaction</Typography>
+            <Grid
+              xs={5}
+              style={{
+                borderStyle: "solid",
+                borderRadius: "1rem",
+              }}
+            >
+              <TransactionCard></TransactionCard>
             </Grid>
-            <Grid xs={7} style={{ borderStyle: "solid" }}>
-              <Typography textAlign="center">Transaction</Typography>
+            <Grid
+              xs={7}
+              style={{
+                borderStyle: "solid",
+                borderRadius: "1rem",
+              }}
+            >
+              <Typography textAlign="center">Transactions</Typography>
               <Btn>Sign</Btn>
               <Btn isBrown={true} width={15}>
                 Cancel
