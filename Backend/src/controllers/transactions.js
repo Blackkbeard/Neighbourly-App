9d04a4252894636fbc71fe652cd5de0992255427
hweebie <limyinhwee@gmail.com>
2023-08-21 10:20:21 +0800

    Add create transaction endpoint

--- Backend/src/controllers/transactions.js
+++ Backend/src/controllers/transactions.js
@@ -3,28 +3,30 @@ const { v4: uuidv4 } = require("uuid");
 const TransactionModel = require("../models/Transactions");
 
 //Seed 2 transactions for test user.
-
 const seedTransactions = async (req, res) => {
   try {
     await TransactionModel.deleteMany();
 
     await TransactionModel.create([
       {
-        listing_id: "64d0f3f75676c304033d8c90",
-        owner_id: "64df45f6d43f6b36609ea557",
-        requester_id: "requester1",
+        _id: "64e2c98f2097aba61989d93c",
+        listing_id: "64d0f3f75676c304033d8c89",
+        owner_id: "64e2c2fcdce21246ef81b8ed",
+        requester_id: "64e2c2ffdce21246ef81b8f4",
         type: "pending_response",
       },
       {
+        _id: "64e2c98f2097aba61989d93d",
         listing_id: "64d0f3f75676c304033d8c89",
-        owner_id: "64df45f6d43f6b36609ea557",
-        requester_id: "requester1",
+        owner_id: "64e2c2fcdce21246ef81b8ed",
+        requester_id: "64e2c2ffdce21246ef81b8f4",
         type: "pending_response",
       },
       {
+        _id: "64e2c98f2097aba61989d93e",
         listing_id: "64d0f3f75676c304033d8c89",
-        owner_id: "64df45f6d43f6b36609ea558",
-        requester_id: "requester1",
+        owner_id: "64e2c2fcdce21246ef81b8ed",
+        requester_id: "64e2c2fcdce21246ef81b8ee",
         type: "accepted",
       },
     ]);
@@ -51,22 +53,73 @@ const getAllTransactions = async (req, res) => {
 // Get transactions by owner's id
 const getTransactionsByOwnerId = async (req, res) => {
   try {
-    const listings = await TransactionModel.find({ owner_id: req.params.id });
+    const transactions = await TransactionModel.find({
+      owner_id: req.params.owner_id,
+    });
 
-    if (listings.length === 0) {
+    if (transactions.length === 0) {
       return res
         .status(400)
         .json({ status: "error", error: "Transactions not found" });
     }
-    res.json(listings);
+    res.json(transactions);
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(400)
+      .json({ status: "error", message: "Cannot get transaction" });
+  }
+};
+
+// Get transactions by requester's's id
+// TODO: debug
+const getTransactionsByRequesterId = async (req, res) => {
+  try {
+    const transactions = await TransactionModel.find({
+      requester_id: req.params.requester_id,
+    });
+
+    if (transactions.length === 0) {
+      return res
+        .status(400)
+        .json({ status: "error", error: "Transactions not found" });
+    }
+    res.json(transactions);
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ status: "error", message: "Cannot get listings" });
   }
 };
 
+//Create new transaction
+const createTransaction = async (req, res) => {
+  try {
+    const createdTransaction = new TransactionModel({
+      owner_id: req.body.owner_id,
+      requester_id: req.body.requester_id,
+      listing_id: req.body.listing_id,
+    });
+    await createdTransaction.save();
+    res.json({
+      status: "ok",
+      msg: "Transaction saved",
+      createdTransaction,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(400)
+      .json({ status: "error", message: "Cannot create transaction" });
+  }
+};
+
+//Update transaction
+//Delete transaction
+
 module.exports = {
   seedTransactions,
   getAllTransactions,
   getTransactionsByOwnerId,
+  getTransactionsByRequesterId,
+  createTransaction,
 };
--- Backend/src/routers/transactions.js
+++ Backend/src/routers/transactions.js
@@ -3,12 +3,17 @@ const {
   seedTransactions,
   getAllTransactions,
   getTransactionsByOwnerId,
+  getTransactionsByRequesterId,
+  createTransaction,
 } = require("../controllers/transactions");
 const checkValid = require("../middleware/checkValid");
 const router = express.Router();
 
 router.get("/transactions/seed", seedTransactions);
 router.get("/transactions", getAllTransactions);
-router.get("/transactions/:id", getTransactionsByOwnerId);
-
+router.get("/transactions/:owner_id", getTransactionsByOwnerId);
+router.get("/transactions/:requester_id", getTransactionsByRequesterId);
+// router.get("/transactions/:listing_id", getTransactionsByOwnerId);
+// ^ TODO: add
+router.put("/transactions", createTransaction);
 module.exports = router;
