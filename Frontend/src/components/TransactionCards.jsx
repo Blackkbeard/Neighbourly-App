4c110323e9bb3548c85d2c365d908fc7c8ecf7d2
hweebie <limyinhwee@gmail.com>
2023-08-21 15:03:56 +0800

    Add card action to transaction card

--- Frontend/src/components/TransactionCards.jsx
+++ Frontend/src/components/TransactionCards.jsx
@@ -14,38 +14,44 @@ const Listings = (props) => {
     <>
       <Card
         key={props.id}
-        sx={{ display: "flex", justifyContent: "space-between" }}
         variant="outlined"
         style={{
           borderRadius: "1rem",
           margin: "0.6rem",
         }}
+        onClick={() => {
+          props.setSelectedTxn(props.id);
+        }}
       >
-        <Box sx={{ display: "flex", flexShrink: 1, flexDirection: "column" }}>
-          <CardContent sx={{ flex: "0 auto" }}>
-            <Avt size={3}></Avt>
-          </CardContent>
-        </Box>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <CardContent sx={{ flex: "0 auto" }}>
-            <Typography component="div" variant="subtitle">
-              {props.listingTitle}
-            </Typography>
-            <Typography
-              variant="subtitle"
-              color="text.secondary"
-              component="div"
-            >
-              {props.requesterName}
-            </Typography>
-          </CardContent>
-        </Box>
-        <CardMedia
-          component="img"
-          sx={{ width: 100 }}
-          image={props.listingImage}
-          alt="Listing img"
-        />
+        <CardActionArea
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Box sx={{ display: "flex", flexShrink: 1, flexDirection: "column" }}>
+            <CardContent sx={{ flex: "0 auto" }}>
+              <Avt size={3}></Avt>
+            </CardContent>
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "0 auto" }}>
+              <Typography component="div" variant="subtitle">
+                {props.listingTitle}
+              </Typography>
+              <Typography
+                variant="subtitle"
+                color="text.secondary"
+                component="div"
+              >
+                {props.requesterName}
+              </Typography>
+            </CardContent>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 100 }}
+            image={props.listingImage}
+            alt="Listing img"
+          />
+        </CardActionArea>
       </Card>
     </>
   );
--- Frontend/src/pages/Transactions.jsx
+++ Frontend/src/pages/Transactions.jsx
@@ -138,50 +138,52 @@ const Transactions = (props) => {
                 sx={{ marginLeft: "5%", marginRight: "5%" }}
               />
               <Box>
-                <Typography
-                  variant="body"
-                  color="text.secondary"
-                  component="div"
-                  display="block"
-                  margin="1rem"
-                >
-                  RequesterName is interested in ListingTitle.
-                </Typography>
-                <Typography
-                  variant="body"
-                  color="text.secondary"
-                  component="div"
-                  display="block"
-                  margin="1rem"
-                >
-                  Accept this request?
-                </Typography>
-                <Typography
-                  variant="body"
-                  color="text.secondary"
-                  component="div"
-                  display="block"
-                  margin="1rem"
-                >
-                  Once accepted, you will exchange mobile numbers to arrange a
-                  meet-up.
-                </Typography>
+                <Box>
+                  <Typography
+                    variant="body"
+                    color="text.secondary"
+                    component="div"
+                    display="block"
+                    margin="1rem"
+                  >
+                    RequesterName is interested in ListingTitle.
+                  </Typography>
+                  <Typography
+                    variant="body"
+                    color="text.secondary"
+                    component="div"
+                    display="block"
+                    margin="1rem"
+                  >
+                    Accept this request?
+                  </Typography>
+                  <Typography
+                    variant="body"
+                    color="text.secondary"
+                    component="div"
+                    display="block"
+                    margin="1rem"
+                  >
+                    Once accepted, you will exchange mobile numbers to arrange a
+                    meet-up.
+                  </Typography>
 
-                <Typography
-                  variant="body"
-                  color="text.secondary"
-                  component="div"
-                  display="block"
-                  margin="1rem"
-                >
-                  Your mobile number: +65 91234567
-                </Typography>
-              </Box>
-              <Box display="flex" margin="1rem">
-                <Btn width={10}>Accept</Btn>
-                <Btn isBrown={true} width={10}>
-                  Decline
-                </Btn>
+                  <Typography
+                    variant="body"
+                    color="text.secondary"
+                    component="div"
+                    display="block"
+                    margin="1rem"
+                  >
+                    Your mobile number: +65 91234567
+                  </Typography>
+                </Box>
+                <Box display="flex" margin="1rem">
+                  <Btn width={10}>Accept</Btn>
+                  <Btn isBrown={true} width={10}>
+                    Decline
+                  </Btn>
+                </Box>
               </Box>
             </Grid>
           </Grid>
