37de33114886807af1a2a4ec1c4fc50e11049dfa
hweebie <limyinhwee@gmail.com>
2023-08-21 11:06:50 +0800

    Update validators

--- Backend/src/validators/transactions.js
+++ Backend/src/validators/transactions.js
@@ -6,21 +6,21 @@ const validateIdInParam = [
 
 const validateCreateTransaction = [
   check("owner_id", "owner_id is required").not().isEmpty(),
-  param("owner_id", "invalid owner_id").isLength({ min: 24, max: 24 }),
+  check("owner_id", "invalid owner_id").isLength({ min: 24, max: 24 }),
   check("requester_id", "requester_id is required").not().isEmpty(),
-  param("requester_id", "invalid requester_id").isLength({ min: 24, max: 24 }),
+  check("requester_id", "invalid requester_id").isLength({ min: 24, max: 24 }),
   check("listing_id", "listing_id is required").not().isEmpty(),
-  param("listing_id", "invalid listing_id").isLength({ min: 24, max: 24 }),
+  check("listing_id", "invalid listing_id").isLength({ min: 24, max: 24 }),
 ];
 
 const validateUpdateTransaction = [
-  param("owner_id", "invalid owner_id")
+  check("owner_id", "invalid owner_id")
     .optional()
     .isLength({ min: 24, max: 24 }),
-  param("requester_id", "invalid requester_id")
+  check("requester_id", "invalid requester_id")
     .optional()
     .isLength({ min: 24, max: 24 }),
-  param("listing_id", "invalid listing_id")
+  check("listing_id", "invalid listing_id")
     .optional()
     .isLength({ min: 24, max: 24 }),
 ];
