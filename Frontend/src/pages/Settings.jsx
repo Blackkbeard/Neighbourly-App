515fff5fde2df827eaa9c44ecf10d8c130490970
Blackkbeard <rvinesh2011@gmail.com>
2023-08-23 01:52:40 +0800

    Settings

--- Frontend/src/pages/Settings.jsx
+++ Frontend/src/pages/Settings.jsx
@@ -16,17 +16,18 @@ import {
 import UserContext from "../context/user";
 import useFetch from "../hooks/useFetch";
 import Btn from "../components/Btn";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 
 const Settings = (props) => {
   const userCtx = useContext(UserContext);
   const userFullInfo = userCtx.userInfo;
   const [openUpdate, setOpenUpdate] = useState(false);
   const [name, setName] = useState("");
-  const [bio, setBio] = useState("");
-  const [number, setNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [zip, setZip] = useState("");
-  const [district, setDistrict] = useState("");
+  const [bio1, setBio1] = useState("");
+  const [number1, setNumber1] = useState("");
+  const [email1, setEmail1] = useState("");
+  const [zip1, setZip1] = useState("");
+  const [district1, setDistrict1] = useState("");
 
   const fetchData = useFetch();
 
@@ -43,13 +44,13 @@ const Settings = (props) => {
 
     const userData = {
       display_name: name,
-      biography: bio,
-      mobile_number: number,
-      email: email,
+      biography: bio1,
+      mobile_number: number1,
+      email: email1,
       location: [
         {
-          district,
-          postal_code: zip,
+          district: district1,
+          postal_code: zip1,
         },
       ],
     };
@@ -112,7 +113,7 @@ const Settings = (props) => {
     const file = event.target.files[0];
     setFile(file);
   };
-
+  JSON.stringify(userCtx);
   return (
     <>
       <TopBar showBurger={true}></TopBar>
@@ -180,12 +181,12 @@ const Settings = (props) => {
                 <Typography gutterBottom variant="h4">
                   Locations :
                 </Typography>
-                <Typography gutterBottom variant="h6">
+                {/* <Typography gutterBottom variant="h6">
                   {userCtx.userInfo.location[0].district}
                 </Typography>
                 <Typography gutterBottom variant="h6">
                   {userCtx.userInfo.location[0].postal_code}
-                </Typography>
+                </Typography> */}
               </Box>
               <Btn
                 startIcon={<ModeEditOutlineOutlinedIcon />}
@@ -223,27 +224,29 @@ const Settings = (props) => {
               <Box xs={2}>
                 <Typography>Email:</Typography>
                 <TextField
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setEmail1(e.target.value)}
                 ></TextField>
               </Box>
               <Box xs={2}>
                 <Typography>Biography :</Typography>
 
-                <TextField onChange={(e) => setBio(e.target.value)}></TextField>
+                <TextField
+                  onChange={(e) => setBio1(e.target.value)}
+                ></TextField>
               </Box>
               <Box xs={2}>
                 <Typography>Mobile Number :</Typography>
                 <TextField
-                  onChange={(e) => setNumber(e.target.value)}
+                  onChange={(e) => setNumber1(e.target.value)}
                 ></TextField>
               </Box>
-              <Box xs={2}>
+              {/* <Box xs={2}>
                 <Typography>Locations :</Typography>
                 <TextField
-                  onChange={(e) => setDistrict(e.target.value)}
+                  onChange={(e) => setDistrict1(e.target.value)}
                 ></TextField>
-                <TextField onChange={(e) => setZip(e.target.value)}></TextField>
-              </Box>
+                <TextField onChange={(e) => setZip1(e.target.value)}></TextField>
+              </Box> */}
             </Box>
           </DialogContentText>
 
